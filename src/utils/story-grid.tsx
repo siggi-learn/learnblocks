35c1d54d2a70d1d68d513766a58b3b99ea7e9358
Malte Hecht <malte.fisch@gmail.com>
2021-04-19 10:39:59 +0200

    simplify story grid

--- src/utils/story-grid.tsx
+++ src/utils/story-grid.tsx
@@ -14,45 +14,47 @@ export const StoryGrid: React.FC<StoryGridProps> = ({
   block,
   presenterState,
 }) => (
-  <div
-    style={{
-      display: "grid",
-      gridTemplateColumns: "50% | 50%",
-      gridTemplateRows: "auto | auto",
-      width: "100%",
-    }}
-  >
-    <div style={{ margin: "0.5rem", gridColumn: 1, gridRow: 1 }}>
-      <strong>Editor:</strong>
-      <br />
-      {editor}
-    </div>
-    <div style={{ margin: "0.5rem", gridColumn: 2, gridRow: 1 }}>
-      <strong>Presenter:</strong>
-      <br />
-      {presenter}
-    </div>
-    <div style={{ margin: "0.5rem", gridColumn: 1, gridRow: 2 }}>
-      <strong>Block: </strong>
-      <br />
-      <textarea
-        readOnly
-        rows={7}
-        style={{ width: "100%" }}
-        value={JSON.stringify(block, null, "  ")}
-      />
-    </div>
-    {presenterState && (
-      <div style={{ margin: "0.5rem", gridColumn: 2, gridRow: 2 }}>
-        <strong>State: </strong>
+  <div style={{ display: "flex", width: "100%" }}>
+    <div style={{ width: "50%", padding: "0.5rem" }}>
+      <div>
+        <strong>Editor:</strong>
+        <br />
+        {editor}
+      </div>
+      <hr />
+      <div>
+        <strong>Block: </strong>
         <br />
         <textarea
           readOnly
           rows={7}
           style={{ width: "100%" }}
-          value={JSON.stringify(presenterState, null, "  ")}
+          value={JSON.stringify(block, null, "  ")}
         />
       </div>
-    )}
+    </div>
+    <div style={{ width: "50%", padding: "0.5rem" }}>
+      <div>
+        <strong>Presenter:</strong>
+        <br />
+        {presenter}
+      </div>
+
+      {presenterState && (
+        <>
+          <hr />
+          <div>
+            <strong>State: </strong>
+            <br />
+            <textarea
+              readOnly
+              rows={7}
+              style={{ width: "100%" }}
+              value={JSON.stringify(presenterState, null, "  ")}
+            />
+          </div>
+        </>
+      )}
+    </div>
   </div>
 )
